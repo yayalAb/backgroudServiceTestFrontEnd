03ee7a4d3b968a2030545fcba4fc421c75910c67
Yayal Abayneh <y.abayneh@appdiv.com>
2023-06-16 16:06:37 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EvaluationApp';
+  res: any;
+  constructor(private http: HttpClient) {}
+  ngOnInit() {}
+
+  requestSentFunc() {
+    this.http.get<any>('http://localhost:8000/true').subscribe((re) => {
+      console.log('request sent ', re);
+    });
+  }
+  CheckConnection() {
+    this.http.get<any>('http://localhost:8000').subscribe((re) => {
+      console.log('request sent ', re);
+    });
+  }
 }
